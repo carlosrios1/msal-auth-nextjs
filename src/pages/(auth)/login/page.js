8634b9f1fd31fe8a02beed70453b0abafd9a5f29
Carlos Rios <krlosrios1@gmail.com>
2023-10-29 14:38:32 -0600

    login listo

--- src/pages/(auth)/login/page.js
+++ src/pages/(auth)/login/page.js
@@ -13,14 +13,8 @@ import Link from "next/link";
 
 export default function Login() {
   const { instance, accounts } = useMsal();
-  const [accountDetails, setAccountDetails] = useState(null);
-  const router = useRouter();
-  const [loginMessage, setLoginMessage] = useState(null);
-  // const [lastActivity, setLastActivity] = useState < number > (Date.now());
 
-  const handleActivity = () => {
-    setLastActivity(Date.now());
-  };
+  const router = useRouter();
 
   if (accounts.length > 0) {
     console.log("accounts", accounts);
@@ -41,22 +35,24 @@ export default function Login() {
       });
   }
 
-  function handleLogout() {
-    instance
-      .logoutPopup(authScopes)
-      .then((response) => {})
-      .catch((e) => {
-        console.log(e);
-      });
-  }
-
   return (
-    <section className="relative">
-      <div className="max-w-6xl mx-auto px-4 sm:px-6">
-        <div className="pt-32 pb-12 md:pt-40 md:pb-20">
-          {/* Page header */}
+
+    <div className="w-full min-h-screen flex justify-center items-center bg-gray-900 animate-fade-down">
+
+      <div className="relative w-[380px] h-[420px] bg-gray-800 rounded-lg overflow-hidden">
+
+        <div className="absolute w-[380px] h-[420px] bg-gradient-to-r from-sky-800 via-sky-800 to-transparent -top-[50%] -left-[50%] animate-spin-slow origin-bottom-right">
+
+        </div>
+
+        <div className="absolute w-[380px] h-[420px] bg-gradient-to-r from-sky-800 via-sky-800 to-transparent -top-[50%] -left-[50%] animate-spin-delay origin-bottom-right">
+
+        </div>
+
+        <div className="absolute inset-1 bg-gray-800 rounded-lg z-10 p-5">
           <div className="max-w-3xl mx-auto text-center pb-12 md:pb-20">
-            <h1 style={{ fontSize: "2.5rem" }}>
+            <h1 className="py-20 text-2xl font-mono">
+
               ¡Bienvenido, es un gusto tenerte de vuelta!
             </h1>
             <AuthenticatedTemplate>
@@ -68,10 +64,10 @@ export default function Login() {
             <UnauthenticatedTemplate>
               <div className="max-w-sm mx-auto">
                 <div className="flex flex-wrap -mx-3">
-                  <div className="w-full">
+                  <div className="flex justify-center items-center  w-full ">
                     <button
                       onClick={() => handleLogin()}
-                      className="btn flex items-center px-7 py-2 text-white bg-sky-950 hover:bg-sky-800 w-full relative"
+                      className="btn flex items-center px-7 text-white bg-sky-700 hover:bg-sky-900 w-full relative"
                     >
                       <svg
                         xmlns="http://www.w3.org/2000/svg"
@@ -105,7 +101,7 @@ export default function Login() {
                         className="h-8 flex items-center border-r border-white border-opacity-60 mr-4 px-2.5"
                         aria-hidden="true"
                       ></span>
-                      <span className="flex-auto items-center-ml-16 px-12">
+                      <span className="flex-auto justify-center items-center-ml-16 p-2 text-sm font-semibold font-mono">
                         Inicia Sesión con Microsoft
                       </span>
                     </button>
@@ -115,7 +111,80 @@ export default function Login() {
             </UnauthenticatedTemplate>
           </div>
         </div>
+
+
       </div>
-    </section>
+
+    </div>
+
+
+
+
+    // <section className="relative">
+    //   <div className="max-w-6xl mx-auto px-4 sm:px-6">
+    //     <div className="pt-32 pb-12 md:pt-40 md:pb-20">
+    //       {/* Page header */}
+    //       <div className="max-w-3xl mx-auto text-center pb-12 md:pb-20">
+    //         <h1 style={{ fontSize: "2.5rem" }}>
+    //           ¡Bienvenido, es un gusto tenerte de vuelta!
+    //         </h1>
+    //         <AuthenticatedTemplate>
+    //           {/* <h6>You're logged in!</h6>
+    //           {accountDetails && <center>Name: {accountDetails.name}</center>}
+    //           <button onClick={() => handleLogout()}>Logout</button> */}
+    //         </AuthenticatedTemplate>
+
+    //         <UnauthenticatedTemplate>
+    //           <div className="max-w-sm mx-auto">
+    //             <div className="flex flex-wrap -mx-3">
+    //               <div className="w-full">
+    //                 <button
+    //                   onClick={() => handleLogin()}
+    //                   className="btn flex items-center px-7 py-2 text-white bg-sky-950 hover:bg-sky-800 w-full relative"
+    //                 >
+    //                   <svg
+    //                     xmlns="http://www.w3.org/2000/svg"
+    //                     width="35"
+    //                     height="24"
+    //                     id="microsoft"
+    //                   >
+    //                     <path
+    //                       fill="#f1511b"
+    //                       d="M11.497 11.497H0V0h11.497Z"
+    //                       data-name="Path 6"
+    //                     ></path>
+    //                     <path
+    //                       fill="#80cc28"
+    //                       d="M24 11.497h-11.5V0h11.5v11.497Z"
+    //                       data-name="Path 7"
+    //                     ></path>
+    //                     <path
+    //                       fill="#00adef"
+    //                       d="M11.497 24H0V12.699h11.497Z"
+    //                       data-name="Path 8"
+    //                     ></path>
+    //                     <path
+    //                       fill="#fbbc09"
+    //                       d="M24 24h-11.5V12.699h11.5v11.301Z"
+    //                       data-name="Path 9"
+    //                     ></path>
+    //                   </svg>
+
+    //                   <span
+    //                     className="h-8 flex items-center border-r border-white border-opacity-60 mr-4 px-2.5"
+    //                     aria-hidden="true"
+    //                   ></span>
+    //                   <span className="flex-auto items-center-ml-16 px-12">
+    //                     Inicia Sesión con Microsoft
+    //                   </span>
+    //                 </button>
+    //               </div>
+    //             </div>
+    //           </div>
+    //         </UnauthenticatedTemplate>
+    //       </div>
+    //     </div>
+    //   </div>
+    // </section>
   );
 }
--- src/pages/about/index.js
+++ src/pages/about/index.js
@@ -4,24 +4,52 @@ import {
   UnauthenticatedTemplate,
 } from "@azure/msal-react";
 import { authScopes } from "../../authConfig";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useRouter } from "next/router";
 
 export default function About() {
   const { instance } = useMsal();
   const router = useRouter();
+  const [isInactive, setIsInactive] = useState(false);
+  let inactivityTimer;
 
   useEffect(() => {
+    const handleUserActivity = () => {
+      console.log("User activity detected."); // Agregar mensaje de depuración
+      setIsInactive(false);
+
+      // Reiniciar el temporizador de inactividad cuando el usuario interactúa con la página.
+      clearTimeout(inactivityTimer);
+
+      // Establecer un nuevo temporizador de inactividad.
+      inactivityTimer = setTimeout(() => {
+        console.log("User is inactive. Logging out..."); // Agregar mensaje de depuración
+        // El usuario está inactivo, realiza el cierre de sesión.
+        handleLogout();
+      }, 10000); // 10 segundos en milisegundos
+    };
+
+    // Agregar manejadores de eventos para rastrear la actividad del usuario.
+    document.addEventListener("mousemove", handleUserActivity);
+    document.addEventListener("keydown", handleUserActivity);
+
     // Verifica si el usuario está autenticado al cargar la página
     if (!instance.getActiveAccount()) {
       // Redirige al usuario a la página de inicio de sesión si no está autenticado
       router.push("/");
     }
+
+    // Limpia los manejadores de eventos al desmontar el componente.
+    return () => {
+      document.removeEventListener("mousemove", handleUserActivity);
+      document.removeEventListener("keydown", handleUserActivity);
+      clearTimeout(inactivityTimer);
+    };
   }, [instance, router]);
 
   function handleLogout() {
     instance
-      .logoutPopup(authScopes)
+      .logoutRedirect(authScopes)
       .then(() => {
         // Redirige al usuario a la página de inicio después del logout exitoso
         router.push("/");
@@ -32,17 +60,24 @@ export default function About() {
   }
 
   return (
-    <div>
-      <h1>About Page</h1>
+    <div className="bg-gray-900 font-mono">
+      <h1 >About Page</h1>
       <AuthenticatedTemplate>
         <h6>You're logged in!</h6>
-        <button onClick={() => handleLogout()}>Logout</button>
+        <div  className="flex justify-center items-center  w-full ">
+          <button className="btn flex items-center px-7 text-white bg-sky-700 hover:bg-sky-900 w-full relative"
+            onClick={() => handleLogout()}>Logout</button>
+        </div>
       </AuthenticatedTemplate>
 
       <UnauthenticatedTemplate>
         <div>
           <h6>You're logged in!</h6>
-          <button onClick={() => handleLogout()}>Logout</button>
+          {isInactive ? (
+            <p>Auto logout in 10 seconds...</p>
+          ) : (
+            <button onClick={() => handleLogout()}>Logout</button>
+          )}
         </div>
       </UnauthenticatedTemplate>
     </div>
